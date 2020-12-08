93c3b510875d584c337456134467cce6f5773006
Benjamin Kern <bkern1@lion.lmu.edu>
2020-12-08 01:02:39 -0800

    this isnt gonna push but should fix the gradient thing

--- canvas.js
+++ canvas.js
@@ -83,7 +83,7 @@
                 document.getElementById('left').classList.remove("active");
                 document.getElementById('down').classList.remove("active");
                 document.getElementById('right').classList.remove("active");
-                if (!document.body.style.background.startsWith('l'))
+                if (!document.body.style.background)
                     document.body.style.background = `linear-gradient(${Math.floor(Math.random()*360)}deg, #${Math.floor(Math.random() * 16777215).toString(16)}, #${Math.floor(Math.random() * 16777215).toString(16)})`;
 
             }, 500);
